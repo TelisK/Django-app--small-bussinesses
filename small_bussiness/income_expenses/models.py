4a7f2acc8d284fbb0fc74b82a5b176459e2885e5
TelisK <tkoutsogiannakis@hotmail.com>
2025-11-26 11:47:58 +0200

    first configurations-corrections

--- small_bussiness/income_expenses/models.py
+++ small_bussiness/income_expenses/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Income(models.Model):
+
+    def __str__(self):
+        return str(self.date) + ' : ' + str(self.income_sum) + ' €'
+    
     income_cash = models.FloatField()
     income_pos = models.FloatField()
     income_sum = models.FloatField()
     date = models.DateField()
-    
